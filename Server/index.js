4ee0e4c846f031b3eddab40d7833b99969eb1f8a
Shahadat Rafi <mdshahadathossenrafi@gmail.com>
2023-09-04 00:46:05 +0600

    db connected by prisma

--- Server/index.js
+++ Server/index.js
@@ -6,12 +6,12 @@ dotenv.config()
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-app.listen(port, ()=> {
-    console.log(`Server is running on ${port}`);
+app.listen(PORT, ()=> {
+    console.log(`Server is running on ${PORT}`);
 });
